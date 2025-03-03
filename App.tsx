a8b52db00b90f6dc818be674bbdb556cf757580c
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2025-03-03 14:04:28 -0800

    Created functions to format data for two stores. Modified scraped data to store data using a Map instead of array. Added isLoading state to hide empty flat list components

--- App.tsx
+++ App.tsx
@@ -18,34 +18,69 @@ import {
 
 import { WebView, WebViewMessageEvent } from "react-native-webview";
 import { Images } from './images/images';
+import { walmartJS, targetJS } from './scripts';
+import { EggInfo } from './types';
 
 
-interface EggInfo {
-  content: string,
-  image: string
+
+const handleWalmartData = (data: string): string[] => {
+
+  let lines = data.split("\n").slice(1, 4);
+  let price: string = lines[0].split(" ").slice(-1)[0]
+  let unitPrice: string = lines[1]
+  let itemName: string = lines[2]
+
+  return [price, unitPrice, itemName]
 }
 
+const handleTargetData = (data: string): string[] => {
 
-const StorePrice = ({content, image}: EggInfo) => {
+  let lines = data.split("\n").slice(0, 5);
+  let price: string = lines[0]
+  let unitPrice: string = lines[0]
+  let itemName: string = lines[1]
 
-  let lines = content.split("\n").slice(1, 4);
-  let price = lines[0].split(" ").slice(-1)
-  let unitPrice = lines[1]
-  let itemName = lines[2]
+  if (lines[0].includes("rated")){
+    let cost_data = lines[1].split("(")
+    price = cost_data[0]
+    unitPrice = cost_data[1].replace(")", "")
+    itemName = lines[2]
+  }
+  else {
+    let cost_data = lines[0].split("(")
+    price = cost_data[0]
+    unitPrice = cost_data[1]?.replace(")", "")
+    itemName = lines[1]
+  }
+  return [price, unitPrice, itemName]
+}
 
+const handleCostcoData = (data: string): string[] => {
+
+
+  
+  return []
+}
+
+const StorePrice = ({content, image, formatContent}: EggInfo) => {
+
+  let data = formatContent(content);
+  let price = data[0]
+  let unitPrice = data[1]
+  let itemName = data[2]
 
   return (
     <View className='bg-gray-700 px-4 py-2 rounded-xl mb-4'>
-      <Text className='text-[18px] text-white'>{itemName}</Text>
+      <Text className='text-[20px] text-white'>{itemName}</Text>
       <View className='flex-row justify-between items-center pt-2'>
         <View>
           <Text className='text-[18px] text-white'>Price: {price}</Text>
           <Text className='text-[18px] text-white'>Per Unit: {unitPrice}</Text>
         </View>
-        <Image 
+        {image && <Image 
           source={{ uri: image}}
           className='h-[64px] w-[64px] rounded-md'
-        />
+        />}
       </View>
     </View>
   )
@@ -55,10 +90,11 @@ const StorePrice = ({content, image}: EggInfo) => {
 const App = () => {
 
   const webViewRef = useRef<WebView>(null);
-  const [scrapedData, setScrapedData] = useState<EggInfo[]>([]);
+  const [scrapedData, setScrapedData] = useState< {[key: string] : EggInfo[]}>({});
   const [lastRetrievedDate, setLastRetrievedDate] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  const stores: String[] = ["https://www.walmart.com/search?q=eggs&typeahead=eggs"];
+  const stores: String[] = ["https://www.walmart.com/search?q=eggs&typeahead=eggs", "https://www.target.com/c/eggs-dairy-grocery/-/N-5xszi"];
 
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -66,47 +102,15 @@ const App = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-
-  const injectedJS = `
-    setTimeout(() => {
-      // Select all paragraph elements and get their text
-      const texts = Array.from(document.querySelectorAll('[data-testid="list-view"]')).slice(1, 3)
-      //.map(el => el.innerText)
-      //.join("\\n\\n"); // Join paragraphs with line breaks
-
-      const result = []
-
-      // Loop over your text elements (assuming texts contains the div elements)
-      for (let i = 0; i < texts.length; i++) {
-        let storeInfo = {};
-
-        // Extract image src (if image exists)
-        let image = texts[i].querySelector("img");
-        storeInfo.image = image ? image.src : null; // Check if image exists before accessing src
-
-        // Extract the content from the span
-        let content = texts[i].querySelector("div");
-        storeInfo.content = content ? content.innerText : null; // Ensure span exists
-
-        // Push the result to the array
-        result.push(storeInfo);
-      }
-
-      // Send the result array to React Native
-      window.ReactNativeWebView.postMessage(JSON.stringify(result));
-
-      // Send data back to React Native
-      //window.ReactNativeWebView.postMessage(result);
-    }, 2000); // Wait 2 seconds to ensure content loads
-  `;
-
   // Handle messages from WebView
-  const handleMessage = (event: WebViewMessageEvent) => {
-  
+  const handleMessage = (storeName: string, event: WebViewMessageEvent) => {
     let currentTime: string = new Date().toDateString();
     console.log(event.nativeEvent.data, currentTime)
     setLastRetrievedDate(currentTime)
-    setScrapedData(JSON.parse(event.nativeEvent.data)); // Update state with scraped data
+    setScrapedData({...scrapedData, [storeName]: JSON.parse(event.nativeEvent.data)}); // Update state with scraped data
+    if(storeName === "Target"){
+      setIsLoading(false)
+    }
   };
 
   return (
@@ -120,50 +124,69 @@ const App = () => {
           source={Images.icon}
           className='w-[40px] h-[40px] rounded-xl mr-4'
         />
-        <Text className='text-green-300 text-[32px] font-bold items-center'>Egg Prices</Text>
+        <Text className='text-green-300 text-[28px] font-bold items-center'>Egg Price Tracker</Text>
       </View>
 
-      {/* <View
-        className='flex flex-row justify-center'
-        >
-        <TextInput 
-          placeholder='Enter your zipcode to find local prices' 
-          placeholderTextColor={"lightgray"} 
-        />
-      </View> */}
-
-      <View className='px-8 py-3'>
-        <TouchableOpacity className='bg-green-400 py-3 flex flex-row justify-center rounded-xl' onPress={() => {
-          setScrapedData([]);
+      <View className='px-12 py-3'>
+        <TouchableOpacity className='bg-green-400 py-2 flex flex-row justify-center rounded-xl' onPress={() => {
+          setScrapedData({});
           console.log("Reloading...");
+          setIsLoading(true)
           webViewRef.current?.reload();
           }}>
-          <Text className='text-[18px] text-gray-800 font-semibold'>Reload Prices</Text>
+          <Text className='text-[20px] text-gray-800 font-semibold'>Reload Prices</Text>
         </TouchableOpacity>
-        {lastRetrievedDate && <Text className='text-[16px] text-green-400 font-semibold'>Last Retrieved: {lastRetrievedDate}</Text>}
+        {lastRetrievedDate && <Text className='text-[16px] mt-1 text-green-400 font-semibold text-center'>Last Retrieved: {lastRetrievedDate}</Text>}
       </View>
 
 
-      {scrapedData.length == 0 && (
+      {scrapedData["Walmart"] === undefined && (
         <View className='flex-1 bg-black'>
+          <Text className='text-center'>Loading Walmart...</Text>
           <WebView
             className='flex-1'
             ref={webViewRef}
             source={{ uri: "https://www.walmart.com/search?q=eggs&typeahead=eggs" }} // Change to your target website
-            injectedJavaScript={injectedJS}
-            onMessage={handleMessage} // Receive messages from WebView
+            injectedJavaScript={walmartJS}
+            onMessage={(e) => handleMessage("Walmart", e)} // Receive messages from WebView
           />
           </View>
       )}
 
+        {scrapedData["Target"] === undefined && <View className='flex-1 bg-black'>
+          <Text className='text-center'>Loading Target...</Text>
+          <WebView
+            className='flex-1'
+            ref={webViewRef}
+            source={{ uri: "https://www.target.com/c/eggs-dairy-grocery/-/N-5xszi" }} // Change to your target website
+            injectedJavaScript={targetJS}
+            onMessage={(e) => handleMessage("Target", e)} // Receive messages from WebView
+          />
+        </View>}
 
-      {scrapedData.length > 0 && <FlatList
-        className='p-4'
-        data={scrapedData}
-        renderItem={({item}) => <StorePrice content={item.content} image={item.image} />}
-        ListHeaderComponent={scrapedData.length > 0 && <Text className='mb-3 font-semibold text-green-400 text-[20px]'>Walmart Egg Prices</Text>}
-        //ListEmptyComponent={<View><Text>No Prices Found</Text></View>}
-      />}
+
+
+      {/* Walmart */}
+      {!isLoading && 
+        <FlatList
+          className='h-[200px] mb-3 px-2'
+          data={scrapedData["Walmart"]}
+          renderItem={({item}) => <StorePrice content={item.content} image={item.image} formatContent={handleWalmartData} />}
+          ListHeaderComponent={<View className={`px-3 py-2 flex-row items-center`} style={{backgroundColor: backgroundStyle.backgroundColor}}><Text className='mb-3 font-semibold text-green-400 text-[20px]'>Walmart Egg Prices</Text></View>}
+          stickyHeaderIndices={[0]}
+        />
+      }
+
+      {/* Target */}
+      {!isLoading &&
+        <FlatList
+          className='h-[200px] px-2'
+          data={scrapedData["Target"]}
+          renderItem={({item}) => <StorePrice content={item.content} image={item.image} formatContent={handleTargetData} />}
+          ListHeaderComponent={<View className={`px-3 py-2 flex-row items-center`} style={{backgroundColor: backgroundStyle.backgroundColor}}><Text className='mb-3 font-semibold text-green-400 text-[20px]'>Target Egg Prices</Text></View>}
+          stickyHeaderIndices={[0]}
+        />
+      }
 
     </SafeAreaView>
   );
