40f6e999480418377ed9d31c74aacf9cc1fb8480
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2025-03-03 17:26:03 -0800

    Modified scripts to scroll down in the webview to increase the likelihood of scraping data

--- scripts.ts
+++ scripts.ts
@@ -1,7 +1,14 @@
 const walmartJS = `
+    window.IntersectionObserver = class {
+        constructor(callback) { this.callback = callback; }
+        observe() { this.callback([{ isIntersecting: true }], this); }
+        unobserve() {}
+        disconnect() {}
+    };
+    window.scrollTo(0, document.body.scrollHeight/2);
     setTimeout(() => {
     // Select all paragraph elements and get their text
-    const texts = Array.from(document.querySelectorAll('[data-testid="list-view"]')).slice(1, 6)
+    const texts = Array.from(document.querySelectorAll('[data-testid="list-view"]')).slice(1, 5)
     //.map(el => el.innerText)
     //.join("\\n\\n"); // Join paragraphs with line breaks
 
@@ -28,15 +35,18 @@ const walmartJS = `
 
     // Send data back to React Native
     //window.ReactNativeWebView.postMessage(result);
-    }, 5000); // Wait 5 seconds to ensure content loads
+    }, 3000); // Wait 5 seconds to ensure content loads
 `;
 
 const targetJS = `
+    window.scrollTo(0, document.body.scrollHeight/3);
+    setTimeout(() => {
+        window.scrollTo(0, document.body.scrollHeight/5);
+    }, 2000)
+
     setTimeout(() => {
     // Select all paragraph elements and get their text
-    const texts = Array.from(document.querySelectorAll('div.styles_ndsCol__MIQSp'))
-    //.map(el => el.innerText)
-    //.join("\\n\\n"); // Join paragraphs with line breaks
+    const texts = Array.from(document.querySelectorAll('div.styles_ndsCol__MIQSp'))//.slice(0, 6)
 
     const result = []
 
@@ -54,16 +64,15 @@ const targetJS = `
 
         // Push the result to the array
         if (storeInfo.image !== null && storeInfo.content !== null){
-            result.push(storeInfo);
+            if (storeInfo.content.includes("Eggs")){
+                result.push(storeInfo);
+            }
         }
     }
 
     // Send the result array to React Native
     window.ReactNativeWebView.postMessage(JSON.stringify(result));
-
-    // Send data back to React Native
-    //window.ReactNativeWebView.postMessage(texts);
-    }, 5000); // Wait 5 seconds to ensure content loads
+    }, 10000); // Wait 8 seconds to ensure content loads
 `;
 
 
