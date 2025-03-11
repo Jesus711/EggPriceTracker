75f4ca84f33492bf6d0294e7090e8ddb399ead0f
Jesus Ruelas-Perez <91504943+Jesus711@users.noreply.github.com>
2025-03-11 09:28:12 -0700

    Removed unused Type

--- components/StorePrice.tsx
+++ components/StorePrice.tsx
@@ -1,13 +1,13 @@
 import { View, Text, Image } from 'react-native'
 import React from 'react'
-import { EggInfo, EggItemInfo } from '../types';
+import { EggItemInfo } from '../types';
 import { styled } from 'nativewind';
 
 const StorePrice = ({title, price, unitPrice, image}: EggItemInfo) => {
 
   return (
     <View className='bg-gray-700 px-4 py-2 m-2 rounded-xl'>
-      <Text className='text-[20px] text-white'>{title}</Text>
+      <Text className='text-[20px] text-white '>{title}</Text>
       <View className='flex-row justify-between items-center pt-2'>
         <View>
           <Text className='text-[18px] text-white'>Price: {price}</Text>
