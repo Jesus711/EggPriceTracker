b914071ac21bad3fb4aef6c41ff3dcf2606f9c6c
Jesus Ruelas <jruelasperez711@gmail.com>
2025-03-12 18:26:09 -0700

    Modified StoreList component to no longer use a FlatList to fix VirtualizedLists error

--- components/StoreList.tsx
+++ components/StoreList.tsx
@@ -1,4 +1,4 @@
-import { View, Text, FlatList, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import { EggItemInfo } from '../types'
 import StorePrice from './StorePrice'
@@ -15,27 +15,32 @@ interface StoreListProps {
 const StoreList = ({ index, name, scrapedData }: StoreListProps) => {
 
     const [isVisible, setIsVisible] = useState(index === 0);
-    //console.log(`LOADED: ${name}`, new Date().toTimeString());
-
-    const toggleListButton = () => (
-        <TouchableOpacity 
-            className={`px-3 py-4 flex-row rounded-xl bg-slate-800`} 
-            onPress={() => setIsVisible((prev) => !prev)}>
-            <View className='w-full flex-row justify-between'>
-                <Text className='font-semibold text-green-400 text-[20px]'>{name} Egg Prices</Text>
-                <Text className='font-semibold text-green-400 text-[20px]'>{scrapedData?.length > 0 ? `${scrapedData.length} items` : "No Items Found"}</Text>
-            </View>
-        </TouchableOpacity>
-    )
 
     return (
-        <FlatList
-            className={`mb-3 px-2 ${isVisible && scrapedData?.length > 0 ? "h-[300px]" : ""}`}
-            data={isVisible ? scrapedData : []}
-            renderItem={({ item }) => <StorePrice title={item.title} price={item.price} unitPrice={item.unitPrice} image={item.image}/>}
-            ListHeaderComponent={toggleListButton}
-            stickyHeaderIndices={[0]}
-        />
+        <View className='p-2'>
+            <TouchableOpacity 
+                className={`border-2 border-slate-600 px-3 py-4 flex-row rounded-xl bg-slate-800 ${isVisible && "bg-green-400"}`} 
+                onPress={() => setIsVisible((prev) => !prev)}>
+                <View className='w-full flex-row justify-between'>
+                    <Text className={`font-semibold ${isVisible ? "text-slate-800": "text-green-400"} text-[20px]`}>{name} Egg Prices</Text>
+                    <Text className={`font-semibold ${isVisible ? "text-slate-800": "text-green-400"} text-[20px]`}>{scrapedData?.length > 0 ? `${scrapedData.length} items` : "No Items Found"}</Text>
+                </View>
+            </TouchableOpacity>
+            {isVisible && scrapedData?.map((item, index) => {
+                console.log(name, item, index)
+                return(
+                <StorePrice key={index} title={item.title} price={item.price} unitPrice={item.unitPrice} image={item.image}/>
+            )})}
+            {isVisible && <TouchableOpacity 
+                className={`mx-10 mt-2 mb-3 py-3 rounded-xl bg-red-400`} 
+                onPress={() => setIsVisible((prev) => !prev)}
+            >   
+                <Text className={`font-bold text-black text-[20px] text-center`}>Close {name} Prices</Text>
+            </TouchableOpacity>}
+
+            {isVisible && <View className='mx-6 mb-3 border-b-gray-400 border-b-4' />}
+
+        </View>
     )
 }
 
